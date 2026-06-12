64be39d33bbb00ecc979cd47b0ecd0157fb03fec
SayanIndra83 <sayanindra0@gmail.com>
2026-06-12 13:35:41 +0530

    issue fixed regarding navbar

--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar.jsx';
-
 export default function AppLayout() {
   return (
     <div className="app-layout">
-      <Navbar />
+      <Navbar/>
       <main className="page-content">
         <Outlet /> 
       </main>
