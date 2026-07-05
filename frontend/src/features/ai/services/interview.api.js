b4d9c8116c3505c05f8feec3415136537d15d4cb
SayanIndra83 <sayanindra0@gmail.com>
2026-07-06 02:32:49 +0530

    Token setting update

--- frontend/src/features/ai/services/interview.api.js
+++ frontend/src/features/ai/services/interview.api.js
@@ -5,6 +5,17 @@ const api = axios.create({
     withCredentials: true
 })
 
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+
 export async function generateReport({resumeFile, selfDescription, jobDescription}){
     try {
         const formData = new FormData();
